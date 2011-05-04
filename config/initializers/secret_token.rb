ec9ba732f021853da7b653a0378da5e0610ed778
vagmi <vagmi.mudumbai@gmail.com>
2011-05-04 20:57:15 +0530

    initial commit - create basic rails project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpershub::Application.config.secret_token = '86350ff2200d6caa982621b235ed2b04abfa44229d705bb2427bc4b8f4b9d6d9c9515ac5fc508406a03c326fb32af1b22ecb55ef5c93b2936a6bd1459285b4f8'

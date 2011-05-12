a50fcf0995b9b301dab7438f73c6c093fa3ec6d1
vagmi <vagmi.mudumbai@gmail.com>
2011-05-12 16:53:35 +0530

    styled listing startups and setup proper slug handling

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,12 +1,14 @@
 class StartupsController < ApplicationController
   before_filter :authenticate_user!, :except=>[:index,:show]
   def index
+    @startups = Startup.order_by(:created_at, :desc).all
   end
   def new
     @startup = current_user.startups.build
   end
   def create
     @startup = Startup.new(params[:startup])
+    @startup.owner=current_user
     if(@startup.save)
       flash[:notice] = "Startup saved successfully"
       redirect_to @startup
@@ -15,7 +17,7 @@ class StartupsController < ApplicationController
     end
   end
   def show
-    @startup = Startup.find(params[:id])
+    @startup = Startup.where(:slug=>params[:id]).first
   end
   def edit
   end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -6,7 +6,26 @@ class Startup
   field :address, :type=>String
   field :country, :type=>String
   field :description, :type => String
+  field :slug, :type=>String
   referenced_in :owner, :class_name=>'User'
   mount_uploader :logo, LogoUploader
   validates_presence_of :name, :url
+  validates_uniqueness_of :slug
+  before_create :set_slug
+
+  def set_slug
+    unless self.slug
+      slug_text = self.name.strip.downcase.gsub(/[^a-z0-9]+/,'-')
+      count=0
+      check_text = slug_text
+      while(Startup.where(:slug=>check_text).count>0)
+        count=count+1
+        check_text = slug_text + "-#{count}"
+      end
+      self.slug = check_text
+    end
+  end
+  def to_param
+    self.slug
+  end
 end

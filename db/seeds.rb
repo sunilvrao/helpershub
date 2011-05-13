3220d3ac332e6c70510b4361417d96e84c412c02
vagmi <vagmi.mudumbai@gmail.com>
2011-05-13 23:29:42 +0530

    add staging data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+%w(introductions technology legal accounting sales strategy).each { |c| Category.create!(:name=>c) }
